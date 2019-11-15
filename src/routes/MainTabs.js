3c7666f4b1ece4e649b583f27d3bfb5ea3b37c2f
reydon227 <dhruvarora@delhinerds.com>
2019-11-15 17:30:52 +0530

    vector icons and base app

--- src/routes/MainTabs.js
+++ src/routes/MainTabs.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import {StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import HomeScreen from '../screens/Home';
 import { createAppContainer } from 'react-navigation';
+import {Icon} from 'native-base';
 
 const TabNavigator = createBottomTabNavigator({
-    Home: HomeScreen,
-    "Contact Lenses": HomeScreen,
-    News: HomeScreen,
-    Support: HomeScreen
+    Home: {
+        screen: HomeScreen,
+        navigationOptions:{
+            tabBarLabel: "Home",
+            tabBarIcon: () =>{
+                return <Icon name="home" style={styles.icon} />
+            }
+        }
+    },
+    Lenses: {
+        screen: HomeScreen,
+        navigationOptions:{
+            tabBarIcon: ()=>{
+                return <Icon type="MaterialIcons" name="toll" style={styles.icon} />
+            }
+        }
+    },
+    Spectacles: {
+        screen: HomeScreen,
+        navigationOptions:{
+            tabBarIcon: () =>{
+                return <Icon type="FontAwesome" name="glasses" style={styles.icon} />
+            }
+        }
+    },
+    Cart: {
+        screen: HomeScreen,
+        navigationOptions:{
+            tabBarIcon: () =>{
+                return <Icon name="cart" style={styles.icon} />
+            }
+        }
+    },
+    Support: {
+        screen:  HomeScreen,
+        navigationOptions:{
+            tabBarIcon: () =>{
+                return <Icon name="chatbubbles" style={styles.icon} />
+            }
+        }
+    }
+})
+
+const styles = StyleSheet.create({
+    icon:{
+        color: "#006984",
+        fontSize: 25        
+    }
 })
 
 export default AppContainer = createAppContainer(TabNavigator);
