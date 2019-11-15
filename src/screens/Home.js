468e3aaee7384c512465cff860d9f8097598f709
reydon227 <dhruvarora@delhinerds.com>
2019-11-15 20:34:39 +0530

    homepage changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import {Container, Content} from 'native-base';
-import {Text} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 import HeaderBar from '../components/HeaderBar';
+import axios from "axios";
 
 class HomeScreen extends React.Component{
-    render(){
-        return(
+
+    constructor(props){
+        super(props);
+        this.state = {   
+            slides: []
+        }        
+    }        
+
+    componentDidMount(){
+        let query = {"query":"{slides:allSliders{id, title, image}}"}
+        axios({
+            method: 'post',
+            url: 'http://localhost:8000/graphql',
+            data: query
+        }).then((response)=>{
+            let slides = response.data.data.slides;
+            this.setState({slides});            
+        })                
+    }
+
+    render(){                
+        return(            
             <Container>
                 <HeaderBar title="Home" />
-                <Content>
-                    <Text>Hello</Text>
+                <Content>                    
+                    <Text>Hello World</Text>
+                    {/* {slides} */}
                 </Content>
             </Container>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    text: {
+        color: "black",
+        fontSize: 100
+    }
+})
 
 export default HomeScreen;
